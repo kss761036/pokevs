2924f5d9f417718ce20eed35e46ff5d9be14dcac
kss761036 <kss761036@gmail.com>
2025-04-30 14:57:07 +0900

    Feat: 리셋 버튼 추가

--- src/PokeFloat.tsx
+++ src/PokeFloat.tsx
@@ -4,6 +4,8 @@ import { useDrop } from "react-dnd";
 import { useRef, useEffect, useState } from "react";
 import { Pokemon } from "./type";
 import PokeDrop from "./PokeDrop";
+import Button from "@mui/material/Button";
+import RestartAltRoundedIcon from "@mui/icons-material/RestartAltRounded";
 
 const PokeFloat = () => {
   const ref1 = useRef<HTMLLIElement>(null);
@@ -11,6 +13,11 @@ const PokeFloat = () => {
 
   const [poke1, setPoke1] = useState<Pokemon | null>(null);
   const [poke2, setPoke2] = useState<Pokemon | null>(null);
+
+  const handleReset = () => {
+    setPoke1(null);
+    setPoke2(null);
+  };
 
   const [{ isOver: isOver1 }, drop1] = useDrop(() => ({
     accept: "POKEMON_CARD",
@@ -47,11 +54,16 @@ const PokeFloat = () => {
             <h4>타입</h4>
           </li>
         </ul>
+        <div className="flex justify-end">
+          <Button onClick={handleReset}>
+            <RestartAltRoundedIcon fontSize="small" sx={{ color: "#000000" }} />
+          </Button>
+        </div>
+        <ul className="flex items-start">
+          <PokeDrop dropped={poke1} onDrop={setPoke1} />
+          <PokeDrop dropped={poke2} onDrop={setPoke2} />
+        </ul>
       </form>
-      <ul className="flex items-start">
-        <PokeDrop dropped={poke1} onDrop={setPoke1} />
-        <PokeDrop dropped={poke2} onDrop={setPoke2} />
-      </ul>
     </div>
   );
 };
